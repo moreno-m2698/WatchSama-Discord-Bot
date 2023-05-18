42fbb9038153301b6888c1d4f58a1fcc4d4c232a
Michael <moreno.m2698@gmail.com>
2023-05-18 12:18:58 -0400

    Added status mapping to api

--- maltest.py
+++ maltest.py
@@ -25,11 +25,11 @@ class MALSeleniumWrapper():
     def __init__(self):
         self.url = 'https://myanimelist.net/'
     
-    def createConnection():
+    def createConnection(self):
         driver = webdriver.Chrome()
         return driver
     
-    def accountLogin(driver,url, username, password):
+    def accountLogin(self, driver, url, username, password):
         driver.get(url)
         loginElement = driver.find_element(By.ID, 'loginUserName')
         passwordElement = driver.find_element(By.ID, 'login-password')
@@ -40,25 +40,33 @@ class MALSeleniumWrapper():
         time.sleep(2)
         driver.get('https://myanimelist.net/animelist/gabslittlepogger')
 
-    def getTitle(element):
-        rawTitle = element.find_element(By.CLASS_NAME, 'title')
-        title = rawTitle.find_element(By.TAG_NAME, 'a').title
-        return title
+    def getEntries(self, driver):
+        animeList = driver.find_elements(By.CLASS_NAME, 'list-item')
+        return animeList
 
+    def getTitle(self, element):
+        rawTitle = element.find_element(By.CLASS_NAME, 'title')
+        title = rawTitle.find_element(By.TAG_NAME, 'a').text
+        return title
+    
+    def getStatus(self, element):
+        statusElement = element.find_element(By.CLASS_NAME, 'status')
+        rawClasses = statusElement.get_attribute('class')
+        classList = list(rawClasses.split(" "))
+        status = classList[2]
+
+        return status
+    
+    def getData(self, driver):
+        rawData = self.getEntries(driver)
+        result = []
+        for element in rawData:
+            refinement = [self.getTitle(element), self.getStatus(element)]
+            result.append(refinement)
         
-    
-    def titlesArray(driver):
-        animeList = driver.find_elements(By.CLASS_NAME, 'list-item') #These are tbody
-        # tbodyTotr = lambda tbody: tbody.find_element(By.CLASS_NAME, 'list-table-data')
-        # trList = list(map(tbodyTotr,animeList))
-        getTdClassTitle = lambda data: data.find_element(By.CLASS_NAME, 'title')
-        rawTitleList = list(map(getTdClassTitle,animeList))
-        getTitle = lambda data: data.find_element(By.TAG_NAME, 'a').text
-        titleList = list(map(getTitle, rawTitleList))
-        print(titleList)
-        time.sleep(2)
-        return titleList
-    
+        return result
+
+
     def findRange(driver): #Assume we are not finding a new webpage
         animeTable = driver.find_elements(By.CLASS_NAME, 'completed')
         parentTest = lambda data: data.parent #This returns the driver
@@ -94,12 +102,13 @@ async def stop(ctx):
 
 async def test(ctx):
     url ='https://myanimelist.net/login.php?from=%2F&'
-    driver = MALSeleniumWrapper.createConnection()
-    MALSeleniumWrapper.accountLogin(driver=driver, url=url, username=mal_username, password=mal_password)
-    #titles = MALSeleniumWrapper.titlesArray(driver=driver) 
-    noWatch = MALSeleniumWrapper.findRange(driver=driver)
+    wrapper=MALSeleniumWrapper()
+    driver = wrapper.createConnection()
+    wrapper.accountLogin(driver=driver, url=url, username=mal_username, password=mal_password)
+    data = wrapper.getData(driver)
     await ctx.send(f"Successful MAL login: Check Log")
-    #await ctx.send(f"Here is the list: {titles}")
+    print(data)
+
     #await ctx.send(f"Here are the no Wathc {noWatch}")
     driver.close()
 
