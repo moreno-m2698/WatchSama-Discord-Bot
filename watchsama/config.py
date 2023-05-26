d5de6687e10808f89a186d7544e4b75062506bf0
Michael <moreno.m2698@gmail.com>
2023-05-26 17:26:27 -0400

    can now make .env calls with config.py methods

--- watchsama/config.py
+++ watchsama/config.py
@@ -1,4 +1,11 @@
-import os
+from dotenv import dotenv_values
 
-import watchsama
 
+def bot_token() -> str:
+    return dotenv_values("..\.env")['BOT_TOKEN']
+
+def mal_user() -> str:
+    return dotenv_values("..\.env")['MAL_USERNAME']
+
+def mal_password() -> str:
+    return dotenv_values("..\.env")['MAL_PASSWORD']
