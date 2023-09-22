e46b087d4d443758ce936c98c5035359e42ac29d
Michael <moreno.m2698@gmail.com>
2023-09-22 15:45:55 -0400

    Refactored code for testing

--- watchsama/cogs/mal/MAL.py
+++ watchsama/cogs/mal/MAL.py
@@ -6,8 +6,7 @@ from discord.ext import commands
 
 
 from .view.MALView import MALView
-from .API.MALController import cache_anime_meta
-from .API.WatchsamaEmbed import make_general_embeds
+
 from .view.HoldView import HoldView
 from .API.RawAnimeData import RawAnimeData
 
@@ -43,27 +42,7 @@ class MALCog(commands.Cog):
         ''' This command will allow user to make a search on MAL and return a list of 5 shows that meet the description'''
         pass
 
-    
 
-
-    @commands.command()
-    async def hold(self, ctx: commands.Context) -> discord.Message:
-        embeds: list[discord.Embed] = make_general_embeds(3)
-        embed_index: int = 0
-        view = HoldView()
-        message: discord.Message = ctx.send(content = 'Here are the series that you have on hold.', embed=embeds[embed_index], view = view)
-        view.message_awareness(message)
-        view.embeds_awareness(embeds)
-        view.embed_index_awareness(embed_index)
-        await message
-
-
-    @commands.command()
-    async def refresh(self, ctx: commands.Context) -> discord.Message: #Allows user to refresh embed list if there was a manual update to MAL after startup
-        keys = ['1', '2', '3', '4', '6']
-        for key in keys:
-            cache_anime_meta(key)
-        await ctx.send("Anime List has been updated")
 
 
 async def cog_setup(bot: commands.Bot):
