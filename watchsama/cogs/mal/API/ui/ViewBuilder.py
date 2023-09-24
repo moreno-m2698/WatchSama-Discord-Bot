baef0fbb5c4d4b32347a85b6756cf2dc98c1d31b
Michael <moreno.m2698@gmail.com>
2023-09-24 13:16:53 -0400

    created load button

--- watchsama/cogs/mal/API/ui/ViewBuilder.py
+++ watchsama/cogs/mal/API/ui/ViewBuilder.py
@@ -10,7 +10,7 @@ class RightButton(Button):
         self._embed_index = embed_index
 
     @property
-    def embed_index(self, interaction: Interaction):
+    def embed_index(self):
         
         return self._embed_index
     
@@ -25,23 +25,38 @@ class LeftButton(Button):
         self._embed_index = embed_index
 
     @property
-    def embed_index(self, interaction: Interaction):
+    def embed_index(self):
         
         return self._embed_index
 
     async def callback(self, interaction: Interaction):
         await interaction.response.edit_message(content="pressed left button")
 
+class LoadButton(Button):
+    def __init__(self, embed_index, style = ButtonStyle.green, label = 'More', disabled = False, custom_id = 'load-button', url = None, emoji = None, row = None):
+        super().__init__(style=style, label=label, disabled=disabled,custom_id=custom_id,url=url,emoji=emoji,row=row)
+        self._embed_index = embed_index
+
+    @property
+    def embed_index(self):
+        
+        return self._embed_index
+
+    async def callback(self, interaction: Interaction):
+        await interaction.response.edit_message(content="pressed More button")
+
 class MALView(View):
     
     def __init__(self, embeds = [], embed_index = 0, data = [], timeout: float | None = 180):
         super().__init__()
-
+        self._embed_index = embed_index
         self._embeds = embeds
         self._data = data
         self._data_index = 0
 
-
+    @property
+    def embed_index(self):
+        return self._embed_index
 
     @property
     def embeds(self):
@@ -60,8 +75,12 @@ class MALView(View):
         self._data_index = data_index
 
     def embed_generator(self) -> list[Embed]:
+        
+        driver = webdriver.Chrome()
 
-        ''' We will use this to create embeds once we need more'''
+        driver.close()
+        
+        
         pass
     
 
@@ -72,6 +91,7 @@ class MALViewBuilder():
     def create_View(embed_index = 0, embeds = [], data:list[list] = []) -> MALView:
         view = MALView(embed_index = embed_index, embeds = embeds, data=data)
         right_button = RightButton(embed_index=0)
+        load_button = LoadButton(embed_index=0)
         left_button= LeftButton(embed_index=0)
         view.add_item(left_button)
         view.add_item(right_button)
